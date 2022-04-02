0a73debf34224cd2da411b03e3c0adae90dfbae9
CryptoSextillionaire <102741568+CryptoSextillionaire@users.noreply.github.com>
2022-04-02 18:09:02 +0700

    Create low_ammo_indicator.lua

---
+++ low_ammo_indicator.lua
@@ -0,0 +1,31 @@
+-- @note: need this font: https://anonfiles.com/jff1w0Pax3/verdana-font-family_zip
+local PositionX = Menu.SliderInt("Text Position", "X", 960, 0, 2000)
+local PositionY = Menu.SliderInt("Text Position", "Y", 500, 0, 2000)
+
+local Timer = GlobalVars.curtime
+local alpha = 255
+
+local FONT = Render.InitFont("Verdana", 20, {"b"})
+
+Cheat.RegisterCallback("draw", function()
+		if not EngineClient.IsConnected() then return end
+		local LocalPlayer = EntityList.GetLocalPlayer()
+		local ClientEntity = EntityList.GetClientEntity(EngineClient.GetLocalPlayer())
+		local GetPlayer = ClientEntity:GetPlayer()
+	
+		if GlobalVars.curtime >= Timer then
+				alpha = alpha - 1
+				if alpha <= 0 then
+						alpha = 255
+					end
+		  end
+	
+		if GetPlayer:IsAlive() then
+		    local Weapon =  LocalPlayer:GetActiveWeapon()
+		    local Ammo = Weapon:GetProp("m_iClip1")
+	
+		        if Ammo < 3 and Ammo > 0 then
+				    Render.Text("RELOAD", Vector2.new(PositionX:Get(), PositionY:Get()), Color.RGBA(255, 0, 0, alpha), 20, FONT)
+				end
+		end
+end)
