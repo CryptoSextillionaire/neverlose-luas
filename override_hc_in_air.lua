f67617a3b8a522702c9d8ccfa2088e19f7907022
CryptoSextillionaire <102741568+CryptoSextillionaire@users.noreply.github.com>
2022-04-04 17:34:42 +0700

    fix overriding when dead

--- override_hc_in_air.lua
+++ override_hc_in_air.lua
@@ -3,18 +3,25 @@ local OverrideValue = Menu.SliderInt("Jumpscout", "In Air Hitchance", 60, 0, 100
 local CachedScoutHitChance = AimbotHitChance:Get()
 
 Cheat.RegisterCallback("draw", function()
-		if not EngineClient.IsConnected() then return end
 		local LocalPlayer = EntityList.GetLocalPlayer()
-		local ClientEntity = EntityList.GetClientEntity(EngineClient.GetLocalPlayer())
-		local GetPlayer = ClientEntity:GetPlayer()
-		local ActiveWeapon = LocalPlayer:GetActiveWeapon()
-
-		if LocalPlayer and GetPlayer:IsAlive() then
-				local InAir = bit.band(LocalPlayer:GetProp("m_fFlags"), bit.lshift(1, 0)) == 0
-				if InAir and ActiveWeapon:GetWeaponID() == 40 then
-						AimbotHitChance:Set(OverrideValue:Get())
-				else
+	
+		if not LocalPlayer or not LocalPlayer:IsAlive() then
+				if  AimbotHitChance:Get() ~= CachedHitChance then
 						AimbotHitChance:Set(CachedScoutHitChance)
+						return
 				end
+		end
+	
+		local ActiveWeapon = LocalPlayer:GetActiveWeapon()
+	
+		if not ActiveWeapon or ActiveWeapon:GetWeaponID() ~= 40 then
+				return
+		end
+
+		local InAir = bit.band(LocalPlayer:GetProp("m_fFlags"), bit.lshift(1, 0)) == 0
+		if InAir then
+				AimbotHitChance:Set(OverrideValue:Get())
+		elseif not InAir then
+				AimbotHitChance:Set(CachedScoutHitChance)
 		end
 end)
